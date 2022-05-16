3d17b65283b520c262f7879780fb70c7d9dfff44
mikejke <mikejke99@gmail.com>
2022-05-16 11:19:14 +0300

    rmv stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import eslint from 'vite-plugin-eslint';
-import stylelint from '@frsource/vite-plugin-stylelint';
+// import stylelint from '@frsource/vite-plugin-stylelint';
 import * as path from 'path';
 
 // https://vitejs.dev/config/
@@ -15,7 +15,7 @@ export default defineConfig({
       sassVariables: 'src/assets/sass/variables.sass',
     }),
     eslint(),
-    stylelint(),
+    // stylelint(),
   ],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
